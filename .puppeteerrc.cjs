71ba1e7d1f29cbed9f11d4d201f75b022fabdf00
Achmad Fadilla <achmadfadilla@yahoo.com>
2023-07-06 12:19:01 +0700

    test

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,5 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: ('/home/sbx_user1051/.cache/puppeteer'),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
